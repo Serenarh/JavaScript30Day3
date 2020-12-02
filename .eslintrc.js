c5e4cfd1545c6c36a71724ea109228d87ae5a43c
Serena Hasler <srhasler@yahoo.com>
2020-12-01 21:08:32 -0600

    Make initial updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,8 +10,9 @@ module.exports = {
   },
   rules: {
     "array-callback-return": "warn",
+    "import/extensions": "warn",
     "no-param-reassign": "warn",
-    "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
+    "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
     "no-unused-expressions": ["error", { allowShortCircuit: true }],
     "no-unused-vars": "warn",
   },
